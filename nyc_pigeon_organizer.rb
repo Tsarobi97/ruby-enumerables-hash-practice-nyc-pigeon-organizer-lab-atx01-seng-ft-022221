674fae8fdb8407291247d9bcfc8c58b3afb54b17
Robert Obi <robobi97@googlemail.com>
2021-02-19 00:25:15 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  
+ final_result = data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, inner_array|
+      inner_array.each do |element|
+        if !final_array[element]
+        final_array[element] = {}
+     end 
+        if !final_array[element][key]
+        !final_array[element][key] = []
+     end
+        final_array[element][key].push(inner_key.to_s)
+    end
+   end
+  end
+ end
+learn submit
