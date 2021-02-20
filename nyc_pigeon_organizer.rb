bca06d3020195de31fd4f5e8aad3bf99888da8e3
Robert Obi <robobi97@googlemail.com>
2021-02-20 01:00:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,18 +2,19 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   
- final_result = data.each_with_object({}) do |(key, value), final_array|
-    value.each do |inner_key, inner_array|
-      inner_array.each do |element|
-        if !final_array[element]
-        final_array[element] = {}
-     end 
-        if !final_array[element][key]
-        !final_array[element][key] = []
-     end
-        final_array[element][key].push(inner_key.to_s)
-    end
+  data.each_with_object({}) do |(key, value), final_hash|
+    value.each do |inner_key, array_of_names|
+      array_of_names.each do |single_name|
+        if !final_hash[single_name]
+        final_hash[single_name] = {}
+      end
+      if !final_hash[single_name][key]
+       !final_hash[single_name][key] = []
+    end 
+        final_hash[single_name][key].push(inner_key.to_s)
    end
   end
  end
-learn submit
+end
+
+
